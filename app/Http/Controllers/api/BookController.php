aaa9517246120d1a68e6da52028e89cb500e6c89
Franklin Saavedra <fjsaavedrah@gmail.com>
2024-01-13 18:33:26 -0500

    Creating api Books and routes

---
+++ app/Http/Controllers/api/BookController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function get()
+    {
+        try {
+            $data = Book::get();
+            return response()->json($data, 200);
+        }
+        catch (\Throwable $th)
+        {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $data['title']          = $request['title'];
+            $data['author']         = $request['author'];
+            $data['genre']          = $request['genre'];
+            $data['isbn']           = $request['isbn'];
+            $data['total_copies']   = $request['total_copies'];
+
+            $res = Book::create($data);
+            return response()->json( $res, 200);
+        }
+        catch (\Throwable $th)
+        {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function getById($id)
+    {
+        try {
+            $data = Book::find($id);
+
+            $response = ["status"=>404,"message"=>"Data not found"];
+
+            if ($data)
+            {
+                return response()->json($data, 200);
+            }
+            else {
+                return response()->json($response, 404);
+            }
+
+        }
+        catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $data['title']          = $request['title'];
+            $data['author']         = $request['author'];
+            $data['genre']          = $request['genre'];
+            $data['isbn']           = $request['isbn'];
+            $data['total_copies']   = $request['total_copies'];
+
+            $response = ["status"=>404,"message"=>"Data not found"];
+            Book::find($id)->update($data);
+            $data = Book::find($id);
+            if ($data)
+            {
+                $response["status"] = 200;
+                $response["message"]= "Updated Succesfully";
+                return response()->json($response, 200);
+            }
+            else {
+                $response["status"] = 404;
+                $response["message"]= "Data not found";
+                return response()->json($response, 404);
+            }
+        }
+        catch (\Throwable $th)
+        {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $data = Book::find($id);
+            if ($data)
+            {
+                $res = Book::find($id)->delete();
+                $response["status"] = 200;
+                $response["message"]= "Deleted Succesfully";
+                return response()->json($response, 200);
+            }
+            else {
+                $response["status"] = 404;
+                $response["message"]= "Data not found";
+                return response()->json($response, 404);
+            }
+        }
+        catch (\Throwable $th)
+        {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/books')->group(function () {
+    Route::get('/',[ BookController::class, 'get']);
+    Route::post('/',[ BookController::class, 'create']);
+    Route::get('/{id}',[ BookController::class, 'getById']);
+    Route::put('/{id}',[ BookController::class, 'update']);
+    Route::delete('/{id}',[ BookController::class, 'delete']);
 });
